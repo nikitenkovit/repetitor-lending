541138216757518ce4414fa86b996f252a94d4df
nikitenkovit <nelot@mail.ru>
2020-12-05 21:39:06 +0300

    add whi-are-we-block.js

---
+++ app/js/why-are-we-block.js
@@ -0,0 +1,50 @@
+"use strict";
+
+(function () {
+  var scrollContainer = document.querySelector('.why-are-we__container');
+  var sliderList = document.querySelector('.why-are-we__list');
+  var allSliderItems = sliderList.children;
+  var indicatorsList = document.querySelector(".why-are-we__slider-indicators");
+  var indicatorsTemplate = indicatorsList.querySelector('.slider-indicators__template');
+  var indicatorsTemplateContent = indicatorsTemplate.content;
+  var allIndicatorsItem = indicatorsList.children;
+
+  /*add indicators on page start*/
+  var addIndicator = function () {
+    var newElement = indicatorsTemplateContent.cloneNode(true);
+    indicatorsList.appendChild(newElement);
+  }
+
+  for (var i = 0; i < allSliderItems.length; i++) {
+    addIndicator();
+  }
+
+  indicatorsTemplate.remove();
+  /*add indicators on page end*/
+
+  allIndicatorsItem[0].classList.add('slider-indicators__item--active');
+
+  scrollContainer.addEventListener('scroll', function (event) {
+    var currentActiveElement = indicatorsList.querySelector('.slider-indicators__item--active');
+
+    currentActiveElement.classList.remove('slider-indicators__item--active');
+
+   var scrolledLeft = event.target.scrollLeft;
+   var currentValue = Math.round((scrolledLeft / sliderList.offsetWidth) * 10);
+
+    if (document.documentElement.clientWidth >= 768) currentValue += 1;
+
+    allIndicatorsItem[currentValue].classList.add('slider-indicators__item--active');
+
+  })
+
+
+
+
+
+
+
+
+
+
+})();
