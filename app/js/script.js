2f1edc93149b5cb63d2fcee6caa3e8d96329b1bd
nikitenkovit <nelot@mail.ru>
2020-12-17 16:25:43 +0300

    add mask for telephone numbers

--- app/js/script.js
+++ app/js/script.js
@@ -97,4 +97,49 @@
     }, timeToShowPopap)
   }
   /*main modal window end*/
+
+  /*mask for phone number start*/
+  var allTelNumber = document.querySelectorAll(".f_phone");
+
+  window.addEventListener("DOMContentLoaded", () => {
+    var setCursorPosition = (pos, elem) => {
+      elem.focus();
+
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+      }
+    }
+
+    function mask() {
+
+      var matrix = "+7 (___) ___ ____";
+      var count = 0;
+      var def = matrix.replace(/\D/g, "");
+      var val = this.value.replace(/\D/g, "");
+      if (def.length >= val.length) val = def;
+      this.value = matrix.replace(/./g, (a) => {
+        return /[_\d]/.test(a) && count < val.length ? val.charAt(count++) : count >= val.length ? "" : a
+      });
+      if (event.type == "blur") {
+        if (this.value.length === 2) this.value = ""
+      } else setCursorPosition(this.value.length, this)
+    }
+
+    var telNumberClickHandler = function (element) {
+      element.addEventListener("input", mask, false);
+      element.addEventListener("focus", mask, false);
+      element.addEventListener("blur", mask, false);
+    }
+
+    for (var i =0; allTelNumber.length; i++) {
+      telNumberClickHandler(allTelNumber[i]);
+    }
+    /*mask for phone number end*/
+  });
 })();
